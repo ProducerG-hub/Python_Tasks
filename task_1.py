a8d7b7d2fcbf5ce3371f1887c3f36634402b7efd
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-01-26 21:34:24 +0300

    Python_Tasks

---
+++ task_1.py
@@ -0,0 +1,9 @@
+# A program to allow a user to enter his/her name and their age and displaying them on the screen
+#using f-string
+
+name = input("What is your name? ")
+age= int(input("Enter your age: "))
+
+print(f"""
+    Hello {name}, you are {age} years old
+""")
